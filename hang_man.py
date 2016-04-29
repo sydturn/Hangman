9b074fc37f9a20aeb4db4ed944088d1fd4dabe2c
Sydney Turnbull <turnbull.sydney@gmail.com>
2016-04-28 20:28:13 -0400

    Added difficulties

--- hang_man.py
+++ hang_man.py
@@ -1,23 +1,41 @@
 from __future__ import print_function
 import random
 
-word_list = []
+easy_word_list = []
+medium_word_list = []
+hard_word_list = []
 file = open('word_list.txt', 'r')
 
 for line in file:
     line = line.rstrip()
-    word_list.append(line)
-
+    if len(line)  <= 4:
+        easy_word_list.append(line)
+    if len(line) > 4 and  len(line)< 7:
+        medium_word_list.append(line)
+    else:
+        hard_word_list.append(line)
 
 while True:
     guesses_left = 6
     play_again = ""
     correct_guessed = []
     wrong_guessed = []
-    word = random.choice(word_list)
+    word = ""
+    difficulty = ""
+
+    while difficulty != "easy" and  difficulty != "medium" and difficulty != "hard":
+        difficulty = str(
+            raw_input("What difficulty would you like to play on: Easy, Medium, or Hard?").lower())
+        if difficulty == "easy":
+            word = random.choice(easy_word_list)
+        elif difficulty == "medium":
+            word = random.choice(medium_word_list)
+        elif difficulty == "hard":
+            word = random.choice(hard_word_list)
+        else:
+            print ("Invalid input, please try again.")
 
     print ("There are", len(word), "letters in the word.")
-    print (word)
     while guesses_left > 0 and len(correct_guessed) != len(word):
         for letter in word:
             if letter.lower() in correct_guessed:
@@ -27,7 +45,7 @@ while True:
         print ()
 
         guess_letter = str(raw_input("Guess a letter: ").lower())
-        if len(guess_letter) != 1:
+        if len(guess_letter) != 1 or not guess_letter.isalpha():
             print ("Invalid input, please try again.")
         elif guess_letter in correct_guessed or guess_letter in wrong_guessed:
             print ("You already guessed that, try again.")
